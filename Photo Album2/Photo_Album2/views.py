5b754c8d0011d0663efa2c21560f83c47be3d8a7
shaked2 <59886599+shaked2@users.noreply.github.com>
2020-03-13 18:08:56 +0200

    Revert "Revert "login and register""
    This reverts commit 02e29027582bd3fab4cbf22c50aa39c2ca945275.

--- Photo Album2/Photo_Album2/views.py
+++ Photo Album2/Photo_Album2/views.py
@@ -4,11 +4,22 @@ Routes and views for the flask application.
 
 from datetime import datetime
 from flask import render_template
-import pandas as pd
 from Photo_Album2 import app
+from Photo_Album2.Models.LocalDatabaseRoutines import create_LocalDatabaseServiceRoutines
+
+from datetime import datetime
+from flask import render_template, redirect, request
+
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
 
 import json 
-#import requests
+import requests
 
 import io
 import base64
@@ -20,7 +31,12 @@ from wtforms import Form, BooleanField, StringField, PasswordField, validators
 from wtforms import TextField, TextAreaField, SubmitField, SelectField, DateField
 from wtforms import ValidationError
 
-from Photo_Album2.Models.QueryFormStructure import QueryFormStructure
+
+from YearEndProject.Models.QueryFormStructure import QueryFormStructure 
+from YearEndProject.Models.QueryFormStructure import LoginFormStructure 
+from YearEndProject.Models.QueryFormStructure import UserRegistrationFormStructure 
+
+db_Functions = create_LocalDatabaseServiceRoutines()
 
 @app.route('/')
 @app.route('/home')
@@ -102,3 +118,47 @@ def data():
         year=datetime.now().year,
         message=' Explanatons of the data ' 
     )
+
+@app.route('/register', methods=['GET', 'POST'])
+def Register():
+    form = UserRegistrationFormStructure(request.form)
+
+    if (request.method == 'POST' and form.validate()):
+        if (not db_Functions.IsUserExist(form.username.data)):
+            db_Functions.AddNewUser(form)
+            db_table = ""
+
+            flash('Thanks for registering new user - '+ form.FirstName.data + " " + form.LastName.data )
+            # Here you should put what to do (or were to go) if registration was good
+        else:
+            flash('Error: User with this Username already exist ! - '+ form.username.data)
+            form = UserRegistrationFormStructure(request.form)
+
+    return render_template(
+        'register.html', 
+        form=form, 
+        title='Register New User',
+        year=datetime.now().year,
+        repository_name='Pandas',
+        )
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def Login():
+    form = LoginFormStructure(request.form)
+
+    if (request.method == 'POST' and form.validate()):
+        if (db_Functions.IsLoginGood(form.username.data, form.password.data)):
+            flash('Login approved!')
+            #return redirect('<were to go if login is good!')
+        else:
+            flash('Error in - Username and/or password')
+   
+    return render_template(
+        'login.html', 
+        form=form, 
+        title='Login to data analysis',
+        year=datetime.now().year,
+        repository_name='Pandas',
+        )
+
